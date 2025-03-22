7d4b41e3d8b361abb64c4156aec7366bb60251f1
dksg07 <thedikesh@gmail.com>
2025-03-22 23:35:59 +0530

    220325

--- bubblesort.cpp
+++ bubblesort.cpp
@@ -1,6 +1,6 @@
 #include<iostream>
 using namespace std;
-void bubblesort(int a[],int n){
+void bubble(int a[],int n){
     for(int i=0;i<n-1;i++){
         for(int j=0;j<n-i-1;j++){
             if(a[j]>a[j+1]){
@@ -15,7 +15,7 @@ int main()
 {
     int a[]={5,4,3,2,1};
     int n=sizeof(a)/sizeof(a[0]);
-    bubblesort(a,n);
+    bubble(a,n);
     for(int i=0;i<n;i++)
         cout<<a[i]<<" ";
     return 0;
---
+++ insertion.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+
+void insertionSort(std::vector<int>& arr) {
+    for (int i = 1; i < arr.size(); ++i) {
+        int key = arr[i];
+        int j = i - 1;
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            --j;
+        }
+        arr[j + 1] = key;
+    }
+}
+
+void printArray(const std::vector<int>& arr) {
+    for (int num : arr) {
+        std::cout << num << " ";
+    }
+    std::cout << std::endl;
+}
+
+int main() {
+    std::vector<int> arr = {12, 11, 13, 5, 6};
+    std::cout << "Original array: ";
+    printArray(arr);
+
+    insertionSort(arr);
+
+    std::cout << "Sorted array: ";
+    printArray(arr);
+
+    return 0;
+}
