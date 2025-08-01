ea4e901e43e6fee0880c7f774eefc7e5136e6a81
dksg07 <thedikesh@gmail.com>
2025-08-01 23:31:27 +0530

    random shit

---
+++ watermelon.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    int w;
+    cin >> w;
+    if (w % 2 == 0 && w > 2) {
+        while(){
+            int a = w/2;
+        }
+        
+        cout <<;
+    } else {
+        cout <<;
+    }
+    return 0;
+}
